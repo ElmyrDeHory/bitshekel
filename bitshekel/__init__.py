8b12268084a912de0cb6efe9ef4cdfd7630de0e6
Elmyr de Hory <direct.the.checkered@gmail.com>
2017-01-18 15:18:38 -0500

    initial commit

---
+++ bitshekel/__init__.py
@@ -0,0 +1,8 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Get your hands off my bitshekels!")
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
